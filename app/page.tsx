68eff4e56f42b5e01ac4d9bb9099fc5d45ae2134
Nedim <mr.ramicevic@gmail.com>
2023-08-29 10:44:54 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -19,14 +19,37 @@ export default function Home() {
 
   return (
     
-    <div className='flex flex-col gap-2 m-2 text-black'>
-      <div className='flex gap-4'>
-        <div>
+    <div className='flex flex-col gap-2 m-2 mx-12 bg-white text-black'>
+      <div>
+        
+      </div>
+      <div className='flex gap-11 mx-6'>
+        <div className='flex gap-3'>
         
 <button onClick={toggle} 
-  className="text-black bg-white hover:bg-gray-300 focus:ring-2 ring-1 ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Display Cols <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+  className="text-black bg-gray-200 hover:bg-gray-300  font-medium rounded-2xl text-sm p-1 px-3 text-center inline-flex content-baseline items-center" type="button">Display 
+   <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg></button>
+  </svg>
+  </button>
+<button  
+  className="text-black bg-gray-200 hover:bg-gray-300  font-medium rounded-2xl text-sm p-1 px-3 text-center inline-flex content-baseline items-center" type="button"> Rating 
+   <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg>
+  </button>
+<button 
+  className="text-black bg-gray-200 hover:bg-gray-300  font-medium rounded-2xl text-sm p-1 px-3 text-center inline-flex content-baseline items-center" type="button">Price 
+   <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg>
+  </button>
+<button 
+  className="text-black bg-gray-200 hover:bg-gray-300  font-medium rounded-2xl text-sm p-1 px-3 text-center inline-flex content-baseline items-center" type="button">Display 
+   <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg>
+  </button>
   {
   show ? (<div id="dropdown" className="z-10 absolute mt-2 bg-gray-300 divide-y divide-gray-100 rounded-lg  w-32 ">
   <ul className="py-2 text-sm text-black " aria-labelledby="dropdownDefaultButton">
